198aa72a79c29aee8607737caf8da44b763f49d4
ob1-dev <chris.obrien@automattic.com>
2022-06-02 12:08:34 +1200

    support building from a path

--- bin/build_and_zip.sh
+++ bin/build_and_zip.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 source ~/.bashrc
 
+if [[ -n $1 ]]; then
+  CWD=`pwd`
+  echo "Changing to $1"
+  cd $1
+fi
+
 poetry install --no-dev
 poetry build
 poetry run pip install --upgrade -t python dist/*.whl
+rm deployment-package.zip
 zip -r deployment-package.zip python -x '*.pyc'
+
+if [[ -n $CWD ]]; then
+  cd $CWD
+fi
