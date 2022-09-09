ac6cac23b6773735f0586d31c2a5d35cfe5cdac2
ob1-dev <chris+github@ob1.nz>
2022-09-09 14:10:24 +1200

    try sourcing bashrc

--- bin/install_poetry.sh
+++ bin/install_poetry.sh
@@ -1,4 +1,3 @@
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
+source .bashrc
 curl -sSL https://install.python-poetry.org | python3 -
 echo 'export PATH="/root/.poetry/bin:$PATH"' >> ~/.bashrc
