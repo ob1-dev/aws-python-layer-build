3a3f0861b4a185a49bcb4f32debe961107bda499
ob1-dev <chris+github@ob1.nz>
2022-09-10 13:43:08 +1200

    catch errors in script

--- bin/install_poetry.sh
+++ bin/install_poetry.sh
@@ -1,3 +1,7 @@
-source .bashrc
+source ~/.bashrc 
 curl -sSL https://install.python-poetry.org | python3 -
+if [[ $? -ne 0  ]] 
+then
+    exit $?
+fi
 echo 'export PATH="/root/.poetry/bin:$PATH"' >> ~/.bashrc
